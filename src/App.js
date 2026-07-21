8ab904bc6c1e658ede9fcf8b2814e7e5f729cead
Wafa Zainab <YOUR_GITHUB_EMAIL@gmail.com>
2026-07-21 22:05:38 +0500

    Week 1 React setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello Wafa!</h1>
+      <h2>My Frontend Internship Journey 2026</h2>
+      <p>This is my first React application.</p>
+    </div>
+  );
+}
+
+export default App;
