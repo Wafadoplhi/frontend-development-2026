0e16d3b4f169bf1cc6ed166acc3af3b5ab7b6669
Wafa Zainab <wafadoplhi@gmail.com>
2026-08-08 19:37:26 +0500

    Fix Pokemon Viewer Axios dependency

--- Week-3/Task-2-Pokemon-Viewer/src/services/pokemonApi.ts
+++ Week-3/Task-2-Pokemon-Viewer/src/services/pokemonApi.ts
@@ -4,7 +4,7 @@ import { Pokemon } from "../types/Pokemon";
 export const fetchPokemon = async (
   name: string
 ): Promise<Pokemon> => {
-  const response = await axios.get<Pokemon>(
+  const response = await axios.get(
     `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`
   );
 
